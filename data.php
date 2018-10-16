f2f6ec61e7e0e2e741fcbfda3293b5a10f46ad44
Nick Ocean <nickocean1997@gmail.com>
2018-10-16 12:13:15 +0300

    data

--- data.php
+++ data.php
@@ -1,5 +1,7 @@
 <?php
 
+phpinfo();
+
 require( "dbconnect.php" );
 $leadId = $_POST['lead_id'];
 $stmt = "SELECT * FROM vicidial_list WHERE lead_id = $leadId";
@@ -9,3 +11,7 @@ $data = mysql_fetch_array($res);
 echo '<pre>';
 print_r($data);
 echo '</pre>';
+
+$userName = 'dev';
+$userApiKey = '3dc80aa0c30f554de82af4ab3924d37316a998cc';
+$url="http://oro.demo";
