f71092a8a13931b93a1b9528fbd48eb3d5dae90b
Nick Ocean <nickocean1997@gmail.com>
2018-10-16 13:20:48 +0300

    data

--- data.php
+++ data.php
@@ -191,6 +191,13 @@ class Relationships {
 		$this->account=['data'=>new Entities('users',$id)];
 	}
 }
+
+function debug($data) {
+	echo "<pre>";
+	print_r($data);
+	echo "</pre>";
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
