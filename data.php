ddb59ee4f5d663093f7f714021c9414ed5ed5d3a
Nick Ocean <nickocean1997@gmail.com>
2018-10-16 11:40:40 +0300

    data

--- data.php
+++ data.php
@@ -1,9 +1,9 @@
 <?php
 
 $leadId = $_POST['lead_id'];
-$stmt = "SELECT * FROM vicidial_list WHERE lead_id = '$leadId'";
+$stmt = "SELECT list_id FROM vicidial_list WHERE lead_id = '$leadId'";
 $data = mysql_query($stmt);
 
-echo '<pre>';
+//echo '<pre>';
 print_r($data);
-echo '</pre>';
+//echo '</pre>';
