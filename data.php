7140ec644087ffca33bb8ef064935ef88250646a
Nick Ocean <nickocean1997@gmail.com>
2018-10-16 11:47:26 +0300

    data

--- data.php
+++ data.php
@@ -1,10 +1,10 @@
 <?php
-print_r($_POST);
 
 $leadId = $_POST['lead_id'];
 $stmt = "SELECT count(*) FROM vicidial_list WHERE lead_id = '$leadId'";
+print_r($stmt);
 $data = mysql_query($stmt);
 
-//echo '<pre>';
+echo '<pre>';
 print_r($data);
-//echo '</pre>';
+echo '</pre>';
