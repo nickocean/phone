e84bf110075b8bc73895e532a3461f756657ecec
Nick Ocean <nickocean1997@gmail.com>
2018-10-16 12:28:51 +0300

    data

--- data.php
+++ data.php
@@ -1,7 +1,5 @@
 <?php
 
-phpinfo();
-
 require( "dbconnect.php" );
 $leadId = $_POST['lead_id'];
 $stmt = "SELECT * FROM vicidial_list WHERE lead_id = $leadId";
@@ -15,3 +13,193 @@ echo '</pre>';
 $userName = 'dev';
 $userApiKey = '3dc80aa0c30f554de82af4ab3924d37316a998cc';
 $url="http://oro.demo";
+
+class Request {
+
+	protected $_url;
+	protected $_username;
+	protected $_apiKey;
+
+	public function __construct($url, $username, $apiUserKey) {
+		$this->_url = $url;
+		$this->_username = $username;
+		$this->_apiKey = $apiUserKey;
+	}
+
+	public function getHeader() {
+		$nonce = hash_hmac('sha512', uniqid(null, true), uniqid(), true);
+		$created = new \DateTime('now', new \DateTimezone('UTC'));
+		$created = $created->format(\DateTime::ISO8601);
+		$digest = sha1($nonce . $created . $this->_apiKey, true);
+		return sprintf(
+			'X-WSSE: UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"', $this->_username, base64_encode($digest), base64_encode($nonce), $created
+		);
+	}
+
+	public function curlPost($path, $data = array()) {
+
+		$wsseHeader[] = "Content-Type: application/vnd.api+json";
+		$wsseHeader[] = $this->getHeader();
+		$options = array(
+			CURLOPT_URL => $this->_url . $path,
+			CURLOPT_HTTPHEADER => $wsseHeader,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HEADER => false,
+			CURLOPT_CUSTOMREQUEST => 'POST'
+		);
+		print_r($options[CURLOPT_URL]);
+		if (isset($data)) {
+			$options += array(
+				CURLOPT_POSTFIELDS => $data,
+				CURLOPT_SAFE_UPLOAD => true
+			);
+		}
+
+		$ch = curl_init();
+		curl_setopt_array($ch, $options);
+		$result = curl_exec($ch);
+
+
+		if (false === $result) {
+			echo curl_error($ch);
+		}
+
+		curl_close($ch);
+		return $result;
+	}
+}
+
+class OroRequest extends Request{
+
+	public function __construct($url ,$u_name, $apikey) {
+		parent::__construct($url ,$u_name, $apikey);
+	}
+
+	public function post($path, $data) {
+		$resp=$this->curlPost($path, \GuzzleHttp\json_encode(['data'=>$data]));
+		return $resp;
+	}
+
+}
+
+class Entities {
+
+	public $type;
+	public $id;
+
+	public function __construct($type, $id) {
+		$this->type = $type;
+		$this->id = $id;
+	}
+}
+
+class EmailsEntities {
+	public $email;
+	public function __construct($email) {
+		$this->email=$email;
+	}
+}
+
+class PhonesEntities{
+	public $phone;
+	public function __construct($phone) {
+		$this->phone=$phone;
+	}
+}
+
+class NewEntities {
+	public $type;
+	public $relationships;
+	public $attributes;
+
+	public function __construct($type, $attributes , $relationships) {
+		$this->type = $type;
+		$this->attributes = $attributes;
+		$this->relationships = $relationships;
+	}
+
+}
+
+class Attributes {
+
+	public $firstName;
+	public $lastName;
+	public $primaryEmail;
+	public $emails;
+	public $phones;
+
+	public function __construct(
+		$firstName, $lastName, $emails, $phones
+	) {
+		$this->emails       = [ $emails ];
+		$this->phones       = [ $phones ];
+		$this->firstName    = $firstName;
+		$this->lastName     = $lastName;
+		$this->primaryEmail = $emails->email;
+	}
+}
+
+class LeadsAttributes extends Attributes {
+
+	public $name;
+
+	public function __construct($name, $firstName, $lastName, $emails, $phones) {
+		parent::__construct($firstName, $lastName, $emails, $phones);
+		$this->name = $name;
+	}
+}
+
+class Relationships {
+
+	public function addOwner($id) {
+		$this->owner=['data'=>new Entities('users',$id)];
+	}
+	public function addOrganization($id) {
+		$this->organization=['data'=>new Entities('organizations',$id)];
+	}
+	public function addStatus($status) {
+		$this->status=['data'=>new Entities('leadstatuses',$status)];
+	}
+	public function addSource($id) {
+		$this->source=['data'=>new Entities('leadsources',$id)];
+	}
+	public function addMethod($id) {
+		$this->method=['data'=>new Entities('method',$id)];
+	}
+	public function addAssignedTo($id) {
+		$this->assignedTo=['data'=>new Entities('users',$id)];
+	}
+	public function addReportsTo($id) {
+		$this->reportsTo=['data'=>new Entities('users',$id)];
+	}
+	public function addAddresses($id) {
+		$this->addresses=['data'=>new Entities('users',$id)];
+	}
+	public function addGroups($id) {
+		$this->groups=['data'=>new Entities('users',$id)];
+	}
+	public function addAccounts($id) {
+		$this->accounts=['data'=>new Entities('users',$id)];
+	}
+	public function addDefaultInAccounts($id) {
+		$this->defaultInAccounts=['data'=>new Entities('users',$id)];
+	}
+	public function addPicture($id) {
+		$this->picture=['data'=>new Entities('users',$id)];
+	}
+	public function addContact($id) {
+		$this->contact=['data'=>new Entities('users',$id)];
+	}
+	public function addOpportunities($id) {
+		$this->opportunities=['data'=>new Entities('users',$id)];
+	}
+	public function addCompaign($id) {
+		$this->compaign=['data'=>new Entities('users',$id)];
+	}
+	public function addCustomer($id) {
+		$this->customer=['data'=>new Entities('users',$id)];
+	}
+	public function addAccount($id) {
+		$this->account=['data'=>new Entities('users',$id)];
+	}
+}
