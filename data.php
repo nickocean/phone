5a6891026613adfef2be9ac1b89c52521a717c7d
Nick Ocean <nickocean1997@gmail.com>
2018-10-17 12:56:41 +0300

    test

--- data.php
+++ data.php
@@ -243,7 +243,7 @@ $userApiKey = '3dc80aa0c30f554de82af4ab3924d37316a998cc';
 $url="http://oro.demo";
 
 // Change status for "Anketa"!!!
-if ($status == 'TS') {
+//if ($status == 'TS') {
 
 	$stmt = "SELECT * FROM vicidial_list WHERE lead_id = $leadId";
 	$res = mysql_query($stmt);
@@ -305,5 +305,5 @@ if ($status == 'TS') {
 	$resp = $crm->post('/index.php/api/calls', $call);
 	debug($resp);
 
-}
+//}
 
