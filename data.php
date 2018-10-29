387d38d86e7ab5bc916830a1a64fcf3ccecb5c41
Nick Ocean <nickocean1997@gmail.com>
2018-10-29 16:07:45 +0200

    fdsfsd

--- data.php
+++ data.php
@@ -244,7 +244,7 @@ $userApiKey = 'ae7f5ab5e44310f9fa398b3ad514c1192e2510b0';
 $url="http://crm.1stlfs.com";
 
 // Change status for "Anketa"!!!
-if ($status == 'Anketa' || $status = 'Test') {
+if ($status == 'Anketa') {
 
 	$stmt = "SELECT * FROM vicidial_list WHERE lead_id = $leadId";
 	$res = mysql_query($stmt);
